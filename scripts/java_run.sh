99a2e97e750a440375d604dc14d01b34e822e050
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2019-01-16 12:29:05 +0200

    fixing java build

--- scripts/java_run.sh
+++ scripts/java_run.sh
@@ -12,20 +12,16 @@ find algorithms/ -name *.java -exec cp {} out/java \;
 echo "building..."
 cd out/java
 for FILE_FULL_NAME in *.java; do
-    echo $FILE_FULL_NAME
-    if grep -q "public static void main" "$FILE_FULL_NAME"; then
-        javac $FILE_FULL_NAME
-    fi
+    javac $FILE_FULL_NAME
 done
 
 echo "running..."
 for FILE_FULL_NAME in *.java; do
-    echo $FILE_FULL_NAME
     if grep -q "public static void main" "$FILE_FULL_NAME"; then
         FILENAME="${FILE_FULL_NAME%%.*}"
         java $FILENAME
     fi
 done
+cd ../../scripts
 
 echo "Java tests COMPLETED"
-cd scripts
