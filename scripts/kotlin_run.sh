ba4ba529bc0a6c71bc9d2718bfa546d6aea0fbb6
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2019-01-21 16:56:30 +0200

    Bash building scripts can accept filetemplate useful for testing

--- scripts/kotlin_run.sh
+++ scripts/kotlin_run.sh
@@ -8,17 +8,28 @@ cd ..
 mkdir -p out/kotlin
 find algorithms/ -name *.kt -exec cp {} out/kotlin \;
 
+key=$1
+if [[ ! $key ]]; then
+    key="*"
+else
+    key="*$key*"
+fi
+echo "The file template: $key"
 
 echo "building..."
 cd out/kotlin
 for FILE_FULL_NAME in *.kt; do
-    FILENAME="${FILE_FULL_NAME%%.*}"
-    kotlinc $FILE_FULL_NAME -include-runtime -d $FILENAME.jar -jvm-target 1.8
+    if [[ $FILE_FULL_NAME == *$key* ]]; then
+        FILENAME="${FILE_FULL_NAME%%.*}"
+        kotlinc $FILE_FULL_NAME -include-runtime -d $FILENAME.jar -jvm-target 1.8
+    fi
 done
 
 echo "running..."
 for FILE_FULL_NAME in *.jar; do
-    java -jar $FILE_FULL_NAME
+    if [[ $FILE_FULL_NAME == *$key* ]]; then
+        java -jar $FILE_FULL_NAME
+    fi
 done
 cd ../../scripts
 
